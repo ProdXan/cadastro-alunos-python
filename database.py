c25c8e3e01aa078e0aaf1042964d09d7f99558b9
ProdXan <rafinha250706@gmail.com>
2026-03-10 06:44:14 -0400

    Implement database connection function
    Added a function to connect to the MySQL database.

--- database.py
+++ database.py
@@ -1,1 +1,10 @@
+import mysql.connector
 
+def conectar():
+    conexao = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="cadastro_alunos"
+    )
+    return conexao
