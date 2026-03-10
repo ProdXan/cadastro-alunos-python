98b648fa3c53284aea60d3e5b8f0b956337542b5
ProdXan <rafinha250706@gmail.com>
2026-03-10 06:43:21 -0400

    Add student registration function in main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from database import conectar
 
+def cadastrar_aluno():
+    nome = input("Nome do aluno: ")
+    nascimento = input("Nascimento: ")
+    cpf = input("CPF: ")
+
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    sql = "INSERT INTO alunos (nome, nascimento, cpf) VALUES (%s,%s,%s)"
+    valores = (nome, nascimento, cpf)
+
+    cursor.execute(sql, valores)
+    conexao.commit()
+
+    print("Aluno cadastrado!")
+
+cadastrar_aluno()
